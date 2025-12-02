b425cba182a8222139152f97c7dc4cfd6d5d3b4b
industrialdev <danigevorkian@gmail.com>
2025-12-02 23:32:16 +0100

    Imported last solution code and updated to net 10.0.100

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,13 +1,15 @@
 using GymManagement.Application.Subscriptions.Commands.CreateSubscription;
+using GymManagement.Application.Subscriptions.Commands.DeleteSubscription;
+using GymManagement.Application.Subscriptions.Queries.GetSubscription;
 using GymManagement.Contracts.Subscriptions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using DomainSubscriptionType = GymManagement.Domain.Subscriptions.SubscriptionType;
 
 namespace GymManagement.Api.Controllers;
 
-[ApiController]
 [Route("[controller]")]
-public class SubscriptionsController : ControllerBase
+public class SubscriptionsController : ApiController
 {
     private readonly ISender _mediator;
 
@@ -19,14 +21,65 @@ public class SubscriptionsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateSubscription(CreateSubscriptionRequest request)
     {
-        var command = new CreateSubscriptionCommand(
+        if (!DomainSubscriptionType.TryFromName(
             request.SubscriptionType.ToString(),
+            out var subscriptionType))
+        {
+            return Problem(
+                statusCode: StatusCodes.Status400BadRequest,
+                detail: "Invalid subscription type");
+        }
+
+        var command = new CreateSubscriptionCommand(
+            subscriptionType,
             request.AdminId);
 
         var createSubscriptionResult = await _mediator.Send(command);
 
-        return createSubscriptionResult.MatchFirst(
-            subscription => Ok(new SubscriptionResponse(subscription.Id, request.SubscriptionType)),
-            error => Problem());
+        return createSubscriptionResult.Match(
+            subscription => CreatedAtAction(
+                nameof(GetSubscription),
+                new { subscriptionId = subscription.Id },
+                new SubscriptionResponse(
+                    subscription.Id,
+                    ToDto(subscription.SubscriptionType))),
+            Problem);
+    }
+
+    [HttpGet("{subscriptionId:guid}")]
+    public async Task<IActionResult> GetSubscription(Guid subscriptionId)
+    {
+        var query = new GetSubscriptionQuery(subscriptionId);
+
+        var getSubscriptionsResult = await _mediator.Send(query);
+
+        return getSubscriptionsResult.Match(
+            subscription => Ok(new SubscriptionResponse(
+                subscription.Id,
+                ToDto(subscription.SubscriptionType))),
+            Problem);
+    }
+
+    [HttpDelete("{subscriptionId:guid}")]
+    public async Task<IActionResult> DeleteSubscription(Guid subscriptionId)
+    {
+        var command = new DeleteSubscriptionCommand(subscriptionId);
+
+        var createSubscriptionResult = await _mediator.Send(command);
+
+        return createSubscriptionResult.Match(
+            _ => NoContent(),
+            Problem);
+    }
+
+    private static SubscriptionType ToDto(DomainSubscriptionType subscriptionType)
+    {
+        return subscriptionType.Name switch
+        {
+            nameof(DomainSubscriptionType.Free) => SubscriptionType.Free,
+            nameof(DomainSubscriptionType.Starter) => SubscriptionType.Starter,
+            nameof(DomainSubscriptionType.Pro) => SubscriptionType.Pro,
+            _ => throw new InvalidOperationException(),
+        };
     }
 }
--- src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -8,28 +8,40 @@ namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
     private readonly ISubscriptionsRepository _subscriptionsRepository;
-    // private readonly IUnitOfWork _unitOfWork;
+    private readonly IAdminsRepository _adminsRepository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository/*, IUnitOfWork unitOfWork*/)
+    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork, IAdminsRepository adminsRepository)
     {
         _subscriptionsRepository = subscriptionsRepository;
-        // _unitOfWork = unitOfWork;
+        _unitOfWork = unitOfWork;
+        _adminsRepository = adminsRepository;
     }
 
     public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
     {
-        // Create a subscription
-        var subscription = new Subscription
+        var admin = await _adminsRepository.GetByIdAsync(request.AdminId);
+
+        if (admin is null)
         {
-            Id = Guid.NewGuid(),
-            SubscriptionType = request.SubscriptionType
-        };
+            return Error.NotFound(description: "Admin not found");
+        }
 
-        // Add it to the database
+        var subscription = new Subscription(
+            subscriptionType: request.SubscriptionType,
+            adminId: request.AdminId);
+
+        if (admin.SubscriptionId is not null)
+        {
+            return Error.Conflict(description: "Admin already has an active subscription");
+        }
+
+        admin.SetSubscription(subscription);
+
         await _subscriptionsRepository.AddSubscriptionAsync(subscription);
-        // await _unitOfWork.CommitChangesAsync();
+        await _adminsRepository.UpdateAsync(admin);
+        await _unitOfWork.CommitChangesAsync();
 
-        // Return subscription
         return subscription;
     }
 }
